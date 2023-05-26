8db1650161e78281184502c9d7991354b077a4a4
kkyeongrokk <sisiko8@naver.com>
2023-05-26 16:07:48 -0700

    Add index, new & create functions

--- controllers/players.js
+++ controllers/players.js
@@ -1,10 +1,21 @@
 const Player = require('../models/player');
 
 module.exports = {
-  index
-}
+  index,
+  new: newPlayer,
+  create
+};
 
 async function index(req, res) {
   const players = await Player.find({});
-  res.render('players/index', {});
+  res.render('players/index', {title: 'Players List', players});
+}
+
+function newPlayer(req, res) {
+  res.render('players/new', {title: 'Register'});
+}
+
+async function create(req, res) {
+  const player = await Player.create(req.body);
+  res.redirect(`/players`);
 }
--- routes/players.js
+++ routes/players.js
@@ -4,5 +4,11 @@ const playersCtrl = require('../controllers/players');
 
 /* GET users listing. */
 router.get('/', playersCtrl.index);
+// GET /players/new
+router.get('/new', playersCtrl.new);
+// POST /players
+router.post('/', playersCtrl.create);
+// GET /players/:id
+// router.get('/:id', playersCtrl.show);
 
 module.exports = router;
