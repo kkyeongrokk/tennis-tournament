e0e318ad3f0c1e697e979984972d6e5a0d9e140d
kkyeongrokk <sisiko8@naver.com>
2023-05-27 17:01:00 -0700

    Create match and tournament models

---
+++ models/tournament.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tournamentSchema = new Schema({
+    firstRound1: {type: Schema.Types.ObjectId, ref: 'Match' },
+    firstRound2: {type: Schema.Types.ObjectId, ref: 'Match' },
+    firstRound3: {type: Schema.Types.ObjectId, ref: 'Match' },
+    firstRound4: {type: Schema.Types.ObjectId, ref: 'Match' },
+    firstRound5: {type: Schema.Types.ObjectId, ref: 'Match' },
+    firstRound6: {type: Schema.Types.ObjectId, ref: 'Match' },
+    firstRound7: {type: Schema.Types.ObjectId, ref: 'Match' },
+    firstRound8: {type: Schema.Types.ObjectId, ref: 'Match' },
+    quarterFinal1: {type: Schema.Types.ObjectId, ref: 'Match' },
+    quarterFinal2: {type: Schema.Types.ObjectId, ref: 'Match' },
+    quarterFinal3: {type: Schema.Types.ObjectId, ref: 'Match' },
+    quarterFinal4: {type: Schema.Types.ObjectId, ref: 'Match' },
+    semiFinal1: {type: Schema.Types.ObjectId, ref: 'Match' },
+    semiFinal2: {type: Schema.Types.ObjectId, ref: 'Match' },
+    final: {type: Schema.Types.ObjectId, ref: 'Match' },
+    
+    
+});
+
+module.exports = mongoose.model('Tournament', tournamentSchema);
