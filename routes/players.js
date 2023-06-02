fd460369dcc5f78f99c4cebeb65ed9e9950dab3c
kkyeongrokk <sisiko8@naver.com>
2023-06-02 09:10:57 -0700

    Add ensuredLoggedIn to routes

--- routes/players.js
+++ routes/players.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 const playersCtrl = require('../controllers/players');
 
 /* GET users listing. */
 router.get('/', playersCtrl.index);
 // GET /players/new
-router.get('/new', playersCtrl.new);
+router.get('/new', ensureLoggedIn, playersCtrl.new);
 // POST /players
-router.post('/', playersCtrl.create);
+router.post('/', ensureLoggedIn, playersCtrl.create);
 
 module.exports = router;
--- routes/tournaments.js
+++ routes/tournaments.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 const tournamentsCtrl = require('../controllers/tournaments');
 
 // GET /tournaments
 router.get('/', tournamentsCtrl.index);
 // GET /tournaments/new
-router.get('/new', tournamentsCtrl.new);
+router.get('/new', ensureLoggedIn, tournamentsCtrl.new);
 // POST /tournaments
-router.post('/', tournamentsCtrl.create);
+router.post('/', ensureLoggedIn, tournamentsCtrl.create);
 // GET /tournaments/register
-router.get('/:id/register', tournamentsCtrl.register);
+router.get('/:id/register', ensureLoggedIn, tournamentsCtrl.register);
 // GET /tournaments/:id
 router.get('/:id', tournamentsCtrl.show);
 // GET /tournaments/:id/draw
@@ -17,14 +18,14 @@ router.get('/:id/draw', tournamentsCtrl.draw);
 // GET /tournaments/:id/players/:playersId
 router.get('/:id/players/:playersId', tournamentsCtrl.showPlayer);
 // DELETE /tournaments/:id
-router.delete('/:id', tournamentsCtrl.delete);
+router.delete('/:id', ensureLoggedIn, tournamentsCtrl.delete);
 // GET /tournaments/:id/edit
-router.get('/:id/edit', tournamentsCtrl.edit);
+router.get('/:id/edit', ensureLoggedIn, tournamentsCtrl.edit);
 // PUT /tournaments/:id
-router.put('/:id', tournamentsCtrl.update);
+router.put('/:id', ensureLoggedIn, tournamentsCtrl.update);
 // GET /tournaments/:id/match/:matchId
 router.get('/:id/match/:matchId', tournamentsCtrl.showMatch);
 // PUT /tournaments/:id/draw/:matchId
-router.put('/:id/draw/:matchId', tournamentsCtrl.updateMatch);
+router.put('/:id/draw/:matchId', ensureLoggedIn, tournamentsCtrl.updateMatch);
 
 module.exports = router;
